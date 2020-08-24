e12a935b0eb433e43dea09a4e3dc7d52fc73d8f4
Håkon Heggedal <hakonheggedal@gmail.com>
2020-08-24 17:07:37 +0200

    first commit, added app and requirements

---
+++ src/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# coding: utf-8
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def entry_point():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
